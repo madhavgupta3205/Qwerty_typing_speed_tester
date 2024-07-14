bf75e8bac5071a9ebab26b214913364d0c0c31b2
Madhav Gupta <madhavgupta3205@gmail.com>
2024-07-14 13:18:03 +0530

    updated files

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+  return (
+    <div className="icons">
+      <div>
+        <span>Made with &hearts; by Madhav</span>
+       
+         
+      </div>
+    </div>
+  );
+}
+
+export default Footer
